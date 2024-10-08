026800dea4a19d0daf6a2aa1f5731ee92b2b9ca2
Brain Ambani <brainambani1@gmail.com>
2024-10-08 16:21:30 +0300

    completed the search and profile section

--- app/dashboard/components/board.tsx
+++ app/dashboard/components/board.tsx
@@ -1,23 +1,23 @@
-import { Calendar, User2Icon } from 'lucide-react';
+import { CalendarDays, User2Icon } from 'lucide-react';
 
 export default function Board() {
   return (
     <main className='flex-1 '>
       <div className=''>
-        <div className='flex justify-between py-6 px-6 '>
+        <div className='flex justify-between items-center py-6 px-6 '>
           <div>
             <input
               type='text'
               placeholder='Search project'
-              className='border-1 w-72 text-primary bg-gray-100 rounded-md p-2 outline-none'
+              className='border-1 w-80 md:w-96 text-primary bg-gray-100 rounded-md p-2 outline-none'
             />
           </div>
           <div className='flex items-center gap-4'>
-            <div className='bg-muted-foreground p-1 cursor-pointer rounded-full'>
-              <Calendar />
+            <div className='bg-gray-200 p-1  cursor-pointer rounded-full'>
+              <CalendarDays />
             </div>
-            <div className='flex items-center gap-1'>
-              <div>
+            <div className='flex items-center rounded-md cursor-pointer gap-1 p-2 hover:bg-slate-200'>
+              <div className='bg-violet-300 rounded-full p-2'>
                 <User2Icon />
               </div>
               <div className='gap-1 pl-2'>
