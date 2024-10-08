f3ac414439add7257aa2fbafe6c90992a1f9e459
Brain Ambani <brainambani1@gmail.com>
2024-10-08 23:06:46 +0300

    added project overview

--- app/dashboard/components/board.tsx
+++ app/dashboard/components/board.tsx
@@ -2,29 +2,31 @@ import { CalendarDays } from 'lucide-react';
 
 import { ModeToggle } from '@/components/mode-toggle';
 import Mainboard from './mainboard';
+import ProjectOverview from './project-overview';
 
 export default function Board() {
   return (
     <main className='flex-1 '>
-      <div className=''>
-        <div className='flex justify-between items-center py-6 px-6 '>
-          <div>
-            <input
-              type='text'
-              placeholder='Search project'
-              className='border-1 w-80 md:w-96 text-primary placeholder-primary bg-gray-100 rounded-md p-2 outline-none'
-            />
-          </div>
-          <div className='flex items-center gap-4'>
-            <div className='bg-gray-200 p-1  cursor-pointer rounded-full'>
-              <CalendarDays />
-            </div>
-            <ModeToggle />
-          </div>
+      <div className='flex justify-between items-center py-6 px-6 '>
+        <div>
+          <input
+            type='text'
+            placeholder='Search project'
+            className='border-1 w-80 md:w-96 text-primary placeholder-primary bg-gray-100 rounded-md p-2 outline-none'
+          />
         </div>
-        <hr />
-        {/* main board */}
+        <div className='flex items-center gap-4'>
+          <div className='bg-gray-200 p-1  cursor-pointer rounded-full'>
+            <CalendarDays />
+          </div>
+          <ModeToggle />
+        </div>
+      </div>
+      <hr />
+      {/* main board */}
+      <div className='flex p-6'>
         <Mainboard />
+        <ProjectOverview />
       </div>
     </main>
   );
---
+++ app/dashboard/components/project-overview.tsx
@@ -0,0 +1,9 @@
+export default function ProjectOverview() {
+  return (
+    <div className='bg-gray-900 flex-1 rounded-lg shadow-lg'>
+      <div className='p-6'>
+        <h1 className='text-xl'>Project Overview</h1>
+      </div>
+    </div>
+  );
+}
