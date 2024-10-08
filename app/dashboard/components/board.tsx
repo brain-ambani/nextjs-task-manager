ab8dc67efea2bf03ce236b85b8fd257e15627ed4
Brain Ambani <brainambani1@gmail.com>
2024-10-08 16:11:55 +0300

    updated the search text and profile

--- app/dashboard/components/board.tsx
+++ app/dashboard/components/board.tsx
@@ -4,12 +4,12 @@ export default function Board() {
   return (
     <main className='flex-1 '>
       <div className=''>
-        <div className='flex justify-between py-6 px-4 '>
+        <div className='flex justify-between py-6 px-6 '>
           <div>
             <input
               type='text'
               placeholder='Search project'
-              className='border-2 w-72 border-gray-200  rounded-md p-2 outline-none'
+              className='border-1 w-72 text-primary bg-gray-100 rounded-md p-2 outline-none'
             />
           </div>
           <div className='flex items-center gap-4'>
@@ -21,7 +21,7 @@ export default function Board() {
                 <User2Icon />
               </div>
               <div className='gap-1 pl-2'>
-                <p className='text-muted-foreground text-xs'>Good morning</p>
+                <p className=' text-xs text-primary'>Good morning</p>
                 <h3 className='font-semibold'>Brain</h3>
               </div>
             </div>
